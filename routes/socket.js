dfa28e152ac47046ff3d9020403cbe081c61831e
frederikbroth <frboe17@student.sdu.dk>
2019-12-15 23:22:55 +0100

    Added namespace

--- routes/socket.js
+++ routes/socket.js
@@ -62,6 +62,6 @@ router.get("/loggedinusers", async (req, res) => {
 router.post("/sendprivate", async (req, res) => {
 	const socketId = req.body.socketId;
 	console.log("Send private Request" + socketId);
-	io.emit("Signal");
+	io.of("/socket").emit("Signal");
 });
 module.exports = router;
